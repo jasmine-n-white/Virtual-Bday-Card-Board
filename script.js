f783235436768f49a78c2b42945855494d9ad082
Jasmine White <jasmine@whitepdx.com>
2024-04-06 17:11:03 -0700

    getting user inputs to display

--- script.js
+++ script.js
@@ -8,14 +8,28 @@ function createCard() {
     let card = document.createElement("div");
     card.classList.add("card");
     const h3Card = document.createElement("h3");
-    h3Card.innterHTML = inputName;
+    h3Card.innerText = inputName;
     card.appendChild(h3Card);
     const pCard = document.createElement("p");
     pCard.innerText = message;
     card.appendChild(pCard);
-    //create img, add the text, append to card
+    const imgCard = document.createElement("img");
+    imgCard.src = imageURL;
+    imgCard.alt = "User submitted image";
+    card.appendChild(imgCard);
     //create buttons, add the text, append to card
-
+    const featureCard = document.createElement("btn");
+    featureCard.innerHTML = `<button class="feature-btn">Feature</button>`;
+    card.appendChild(featureCard);
+    const deleteCard = document.createElement("btn");
+    deleteCard.innerHTML = `<button class="delete-btn">Delete</button>`;
+    card.appendChild(deleteCard);
+    //event listeners for the feature and delete buttons
+    // btn.querySelector(".feature-btn").addEventListener("click", function() {
+        
+    // });
+    // btn.querySelector(".delete-btn").addEventListener("click", function() {
+    // });
     //append card to grid
     document.getElementById("cardGrid").appendChild(card);
 }
@@ -24,3 +38,5 @@ myForm.addEventListener("submit", function(event) {
     event.preventDefault();
     createCard();
 });
+
+
