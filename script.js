8f67bb953846b4ec03386da52f2bc1b9c1702d8c
Jasmine White <jasmine@whitepdx.com>
2024-04-14 13:46:16 -0700

    finished form validization and am working on css now

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function createCard() {
     let card = document.createElement("div");
     card.classList.add("card");
     const h3Card = document.createElement("h3");
-    h3Card.innerText = inputName;
+    h3Card.innerText = `From: ${inputName}`;
     card.appendChild(h3Card);
     const pCard = document.createElement("p");
     pCard.innerText = message;
@@ -18,12 +18,15 @@ function createCard() {
     imgCard.alt = "User submitted image";
     card.appendChild(imgCard);
     //create buttons, add the text, append to card
+    const buttonSection = document.createElement("div");
+    buttonSection.classList.add("button-section");
+    card.appendChild(buttonSection);
     const featureCard = document.createElement("btn");
-    featureCard.innerHTML = `<button class="feature-btn">Feature</button>`;
-    card.appendChild(featureCard);
+    featureCard.innerHTML = `<button class="feature-btn">✨Feature✨</button>`;
+    buttonSection.appendChild(featureCard);
     const deleteCard = document.createElement("btn");
-    deleteCard.innerHTML = `<button class="delete-btn">Delete</button>`;
-    card.appendChild(deleteCard);
+    deleteCard.innerHTML = `<button class="delete-btn">❌Delete❌</button>`;
+    buttonSection.appendChild(deleteCard);
     //event listeners for the feature and delete buttons
     card.querySelector(".feature-btn").addEventListener("click", function() {
         card.classList.toggle("feature");
@@ -36,33 +39,64 @@ function createCard() {
 }
 
 //form validation
+function isAlpha(str) {
+    return /^[A-Za-z]+$/.test(str);
+  }
+
+function isURL(str) {
+    return /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+}
 
 
 function checkForm() {
     let inputName = document.getElementById("name");
     let message = document.getElementById("message");
     let imageURL = document.getElementById("image");
+
+
+
+    if (inputName.validity.valueMissing) {
+        inputName.setCustomValidity("Please enter a name!");
+     } else if (!isAlpha(inputName.value)) {
+        inputName.setCustomValidity("Name can only contain letters!");
+        inputName.value = null;
+     } else {
+        inputName.setCustomValidity("");
+     }
+
+     if (message.validity.valueMissing) {
+        message.setCustomValidity("Please enter a message!");
+     } else {
+        message.setCustomValidity("");
+     }
+
+     if (imageURL.validity.valueMissing) {
+        imageURL.setCustomValidity("Please enter an image or GIF URL!");
+     }  else if (imageURL.validity.typeMismatch){
+        imageURL.setCustomValidity("Invalid input! You must enter a valid image or GIF URL.");
+     } else {
+        imageURL.setCustomValidity("");
+     }
+  
+
+
+
+
+ }
+
+     
     
-    validateName(inputName);
-    // validateMessage(message);
-    // validateImage(imageURL);
-}
+
+
+
+
 
 myForm.addEventListener("submit", function(event) {
     event.preventDefault();
+    checkForm();
     createCard();
+    this.reset();
 });
 
-function isAlpha(str) {
-    return /^[A-Za-z]+$/.test(str);
-  }
 
-function validateName(input) {
-    if (input.validity.valid) {
-        if (!isAlpha(input.value)) {
-            inputName.setCustomValidity("Name can only contain letters!");
-        }
-    } else {
-        inputName.setCustomValidity("Please enter a name!");
-    }
-}
+
